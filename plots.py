63b197d5bd1ff1f489562e7a60fbbefd64837f79
MrX1997 <ja.saavedra10@uniandes.edu.co>
2018-11-14 07:58:05 -0500

    Ejercicio MCA 27 final

--- plots.py
+++ plots.py
@@ -3,9 +3,9 @@ matplotlib.use("Agg")
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-n=float(sys.argv[1])
-mu=float(sys.argv[2])
-sigma=float(sys.argv[3])
+n=500
+mu=0
+sigma=1
 
 from scipy.stats import norm
 
@@ -17,20 +17,24 @@ d4=np.genfromtxt('sample_4.dat')
 
 x_axis = np.arange(-10, 10, 0.001)
 plt.subplot(221)
-plt.plot(x_axis, norm.pdf(x_axis,mu,sigma))
+plt.plot(x_axis, norm.pdf(x_axis,mu,sigma),label='sample 1')
 plt.hist(d1,normed=1)
+plt.legend()
 
 plt.subplot(222)
-plt.plot(x_axis, norm.pdf(x_axis,mu,sigma))
+plt.plot(x_axis, norm.pdf(x_axis,mu,sigma),label='sample 2')
 plt.hist(d2,normed=1)
+plt.legend()
 
 plt.subplot(223)
-plt.plot(x_axis, norm.pdf(x_axis,mu,sigma))
+plt.plot(x_axis, norm.pdf(x_axis,mu,sigma),label='sample 3')
 plt.hist(d3,normed=1)
+plt.legend()
 
 plt.subplot(224)
-plt.plot(x_axis, norm.pdf(x_axis,mu,sigma))
+plt.plot(x_axis, norm.pdf(x_axis,mu,sigma),label='sample 4')
 plt.hist(d4,normed=1)
+plt.legend()
 
 plt.title('Sample vs Real')
 plt.savefig('sample.pdf')
